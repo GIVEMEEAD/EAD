0ca1c823a32bc268df83a617708201c223f6565c
GIVEMEEAD <77602153+GIVEMEEAD@users.noreply.github.com>
2021-01-18 00:39:54 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -2,16 +2,27 @@ from selenium import webdriver
 import os
 import random
 import time
+from fastapi import FastAPI,BackgroundTasks
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.responses import JSONResponse
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
+def run_refresher():
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
 
-# browser_list = [webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),chrome_options=chrome_options) for _ in range(2)]
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),chrome_options=chrome_options)
-while True:
-     driver.refresh()
-     print("browser refreshed")
-     time.sleep(random.randint(2,10))
+    # browser_list = [webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),chrome_options=chrome_options) for _ in range(2)]
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),chrome_options=chrome_options)
+    while True:
+        driver.refresh()
+        print("browser refreshed")
+        time.sleep(random.randint(2,10))
+
+app = FastAPI()
+
+@app.get("/")
+def run(back:BackgroundTasks):
+    back.add_task(run_refresher)
+    return JSONResponse('True')
