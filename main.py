a9b7f162be92ce52b5d831446361c1a614d578ab
GIVEMEEAD <77602153+GIVEMEEAD@users.noreply.github.com>
2021-01-18 00:21:57 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from selenium import webdriver
 import os
+import random
+import time
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),chrome_options=chrome_options)
-driver.get("https://www.youtube.com/watch?v=MZl3sTOIW1E&feature=youtu.be")
-print(len(driver.page_source))
-driver.close()
+
+browser_list = [webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),chrome_options=chrome_options) for _ in range(5)]
+# driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),chrome_options=chrome_options)
+while True:
+     browser_num = random.randint(len(browser_list))
+     browser_list[browser_num].refresh()
+     print("browser number", browser_num, "refreshed")
+     time.sleep(random.randint(2,10))
