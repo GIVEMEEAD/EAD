470e89136b5b978227b4995ea70519345bd357ed
GIVEMEEAD <77602153+GIVEMEEAD@users.noreply.github.com>
2021-01-17 22:47:45 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+import os
+
+chrome_options = webdriver.chrome_options()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+driver = webdriver.Chrome(excutable_path=os.environ.get("CHROMEDRIVER_PATH"),chrome_options=chrome_options)
+driver.get("https://www.youtube.com/watch?v=MZl3sTOIW1E&feature=youtu.be")
