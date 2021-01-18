bd2dae9ca109f2aa76062febebe6f968616a4300
GIVEMEEAD <77602153+GIVEMEEAD@users.noreply.github.com>
2021-01-18 00:28:34 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -9,10 +9,9 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 
-browser_list = [webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),chrome_options=chrome_options) for _ in range(5)]
-# driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),chrome_options=chrome_options)
+# browser_list = [webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),chrome_options=chrome_options) for _ in range(2)]
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),chrome_options=chrome_options)
 while True:
-     browser_num = random.randint(len(browser_list))
-     browser_list[browser_num].refresh()
-     print("browser number", browser_num, "refreshed")
+     driver.refresh()
+     print("browser refreshed")
      time.sleep(random.randint(2,10))
